afc3b5da30c2d14715b09055e9e917fccfb96d58
Habit-Tracker <sauravbista10@gmail.com>
2024-08-02 20:38:03 +0545

    added review links for all the movies

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ movies = response.text
 soup = BeautifulSoup(movies, "html.parser")
 list_of_movies = soup.find_all(name="h3", class_="listicleItem_listicle-item__title__BfenH")
 movie_names = [movie.getText() for movie in list_of_movies]
+link_of_movies = soup.find_all("p")
+
+# Extract the href and text from each anchor tag
+movie_links = [(a['href'], a.text) for p in link_of_movies for a in p.find_all('a')]
+
+
+
+
 movie_names.reverse()
 with open("movie_names.txt", "w") as names:
     for movie_name in movie_names:
         names.write(movie_name+ '\n')
 
-print()
 
-# article_link = soup.get("href")
+with open("movie_names.txt", "r") as movies:
+    movie = movies.read()
+    print(movie)
+article_link = soup.get("href")
+with open("movie_link.txt", "w") as links:
+    for link in movie_links:
+        links.write(f"{link[0]} \n")
 
 # print(article_link)
 # score = soup.find(name="span", class_="score").getText()
